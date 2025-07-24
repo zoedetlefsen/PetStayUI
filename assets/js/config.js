3e91872fca62007198698156e86f52f127b00a90
zoedetlefsen <122552548+zoedetlefsen@users.noreply.github.com>
2025-07-23 23:00:44 -0400

    updated aws region

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.PETSTAY_CONFIG = {
-  AWS_REGION: "{{AWS_REGION}}",
-  COGNITO_USER_POOL_ID: "{{COGNITO_USER_POOL_ID}}",
-  COGNITO_USER_POOL_CLIENT_ID: "{{COGNITO_USER_POOL_CLIENT_ID}}",
-  COGNITO_DOMAIN: "{{COGNITO_DOMAIN}}",
+  AWS_REGION: "us-east-2",
+  COGNITO_USER_POOL_ID: 'us-east-2_BmxAayR3h',
+  COGNITO_USER_POOL_CLIENT_ID: 'drf7sts1ns90ip3hpch8h1gr1',
+  COGNITO_DOMAIN: 'https://us-east-2bmxaayr3h.auth.us-east-2.amazoncognito.com',
   REDIRECT_SIGN_IN_URL: "{{REDIRECT_SIGN_IN_URL}}",
   REDIRECT_SIGN_OUT_URL: "{{REDIRECT_SIGN_OUT_URL}}",
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
@@ -17,3 +17,4 @@ for (const key in window.PETSTAY_CONFIG) {
     throw new Error(`Missing config value: ${key}. Did you forget to set environment variables?`);
   }
 }
+
