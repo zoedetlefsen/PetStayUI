a8a39c5ffe9d138c9907d89fa7f0dbd5d9a53c69
zoedetlefsen <122552548+zoedetlefsen@users.noreply.github.com>
2025-08-03 16:15:29 -0400

    updated config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,14 +1,14 @@
 window.PETSTAY_CONFIG = {
   AWS_REGION: "us-east-2",
-  COGNITO_USER_POOL_ID: 'us-east-2_BmxAayR3h',
-  COGNITO_USER_POOL_CLIENT_ID: 'drf7sts1ns90ip3hpch8h1gr1',
-  COGNITO_DOMAIN: 'https://us-east-2bmxaayr3h.auth.us-east-2.amazoncognito.com',
-  REDIRECT_SIGN_IN_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/admin-frontend/post-login.html",
-  REDIRECT_SIGN_OUT_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/index.html",
+  COGNITO_USER_POOL_ID: 'us-east-2_xY7Mf98Hk',
+  COGNITO_USER_POOL_CLIENT_ID: 'acrr8036vgbsdlnv2pg1rploa',
+  COGNITO_DOMAIN: 'https://us-east-2xy7mf98hk.auth.us-east-2.amazoncognito.com',
+  REDIRECT_SIGN_IN_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/admin-frontend/post-login.html',
+  REDIRECT_SIGN_OUT_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/index.html',
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
   ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
   NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
-  CHECKIN_API_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/checkin.html"
+  CHECKIN_API_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/checkin.html',
 };
 
 // Safety check: crash the page if placeholders were not replaced
