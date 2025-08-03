50c1d7849e441554fc057c4d56bd487f3e69d5fd
zoedetlefsen <122552548+zoedetlefsen@users.noreply.github.com>
2025-08-03 17:44:35 -0400

    edited config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,10 +5,10 @@ window.PETSTAY_CONFIG = {
   COGNITO_DOMAIN: 'https://us-east-2xy7mf98hk.auth.us-east-2.amazoncognito.com',
   REDIRECT_SIGN_IN_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/admin-frontend/post-login.html',
   REDIRECT_SIGN_OUT_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/index.html',
-  BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
+  BOOKINGS_API_URL: 'https://t8vz2fo1n8.execute-api.us-east-2.amazonaws.com/prod',
   ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
-  NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
-  CHECKIN_API_URL: 'https://main.d1f86unaot6kvq.amplifyapp.com/checkin.html',
+  NEW_BOOKING_API_URL: 'https://t8vz2fo1n8.execute-api.us-east-2.amazonaws.com/prod',
+  CHECKIN_API_URL: '{CHECKIN-API-URL}}',
 };
 
 // Safety check: crash the page if placeholders were not replaced
