6f6a692326d21ada1bdfea0477442ea23de50fae
zoedetlefsen <122552548+zoedetlefsen@users.noreply.github.com>
2025-08-02 21:54:03 -0400

    added sign in, sign out, check in url

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,12 +3,12 @@ window.PETSTAY_CONFIG = {
   COGNITO_USER_POOL_ID: 'us-east-2_BmxAayR3h',
   COGNITO_USER_POOL_CLIENT_ID: 'drf7sts1ns90ip3hpch8h1gr1',
   COGNITO_DOMAIN: 'https://us-east-2bmxaayr3h.auth.us-east-2.amazoncognito.com',
-  REDIRECT_SIGN_IN_URL: "{{REDIRECT_SIGN_IN_URL}}",
-  REDIRECT_SIGN_OUT_URL: "{{REDIRECT_SIGN_OUT_URL}}",
+  REDIRECT_SIGN_IN_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/admin-frontend/post-login.html",
+  REDIRECT_SIGN_OUT_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/index.html",
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
   ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
   NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
-  CHECKIN_API_URL: "{{CHECKIN_API_URL}}"
+  CHECKIN_API_URL: "https://main.d1r2wj45a086eo.amplifyapp.com/checkin.html"
 };
 
 // Safety check: crash the page if placeholders were not replaced
